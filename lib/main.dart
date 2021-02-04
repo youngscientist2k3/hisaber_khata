a4a3351796c3e98f3a696dcb98a17c7ea7e9af48
sushovan <youngscientist2k3@gmail.com>
2021-02-04 19:00:48 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:hisaber_khata/routes/routes.dart';
+import 'package:hisaber_khata/screens/statement_page.dart';
+import 'package:hisaber_khata/themes/app_theme.dart';
+import 'package:provider/provider.dart';
+import 'package:hisaber_khata/brain/app_brain.dart';
+
+void main() {
+  runApp(ChangeNotifierProvider<AppBrain>(
+    create: (context) => AppBrain(),
+    child: HisaaberKhata(),
+  ));
+}
+
+class HisaaberKhata extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      // theme: appTheme,
+      home: Scaffold(
+        appBar: Appbar(),
+      )
+      // initialRoute: StatementPage.id,
+      routes: routes,
+    );
+  }
+}
